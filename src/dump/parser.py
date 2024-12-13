578ab793a9bda4d6fec622e292b9efaba866ac6a
tito <alb.pascau@gmail.com>
2024-12-13 14:57:35 +0100

    Finished the process_file function. This function takes a file and processes it into data that later the ASTAR can use

--- src/dump/parser.py
+++ src/dump/parser.py
@@ -1,27 +1,17 @@
-import sys 
-import re
+import sys
+import re 
 
-def process_file() -> tuple[int,list,list]:
-    """
-    This function processes a file with the format 
-    specified and returns a tuple with the data. 
-    :return (int): Number of planes
-    :return (list): Each plane initial and final position. 
-        + First plane has the data in position 0 and so on
-    :return (list): Map of the aeroport. Each row in the input
-        is represented as a list. 
-    """
-    planeNumber = 0
-    planePositions =[]
-    map = []
+def process_file():
     # Get data file
     filename = sys.argv[1]
     # Read the file
     try:
         with open(filename, 'r') as file:
             lines = file.readlines()
+
         planeNumber = int(lines[0])
 
+        planePositions = []
         for i in range(1,planeNumber+1):
             # Get the (X,X) (X,X) format as 
             #two pairs of values in the list using regex
@@ -39,6 +29,7 @@ def process_file() -> tuple[int,list,list]:
             else:
                 print(f"ERROR: {filename} -> Position formatting seems to be wrong")
 
+        map = []
         for i in range(planeNumber+1,len(lines)):
             pattern =  r"[ABG]"
             notPattern= r"[^ABG;\n]"
@@ -49,20 +40,15 @@ def process_file() -> tuple[int,list,list]:
             else: 
                 if match:
                     map.append(match)
-        
-        return planeNumber,planePositions,map
-        
+
+        print(planeNumber)
+        print(planePositions)
+        print(map)
 
     except FileNotFoundError:
         print(f"ERROR: The file {filename} was not found")
         sys.exit(1)
     except ValueError:
         print(f"ERROR: THe file {filename} is not properly encoded")
-        sys.exit(1)
 
-
-def main():
-    process_file()
-
-
-
+process_file()
